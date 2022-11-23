9c524281a3befd87cdbab0604c990526d69f4797
Riteesh Pai <riteeshpai2002@gmail.com>
2022-11-23 09:18:35 +0530

    Headings

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useState} from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import './Nav.css'
+import VConnectfinal from '../../VConnectfinal.svg'
+// BEM = Block Element Modifier
 
 const Nav = () => {
-  return (
-    <div>
-          Nav 
-    </div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Nav
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img style={{height:50, width:200}} src={VConnectfinal
+          } alt="logo"/>
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">About VConnect</a></p>
+          <p><a href="#features">HowToUse</a></p>
+          <p><a href="#blog">ContactUs</a></p>
+          <p><a href="#possibility">WorkWithUs</a></p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is VConnect?</a></p>
+            <p><a href="#possibility">Contack Us</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
